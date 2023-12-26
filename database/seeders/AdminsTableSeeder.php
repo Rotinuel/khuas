b5b7f41544e6fccfdb57f087c19fba8f64aa5c3a
Immanuel Okhuarobo <emmanuelisagenius@gmail.com>
2023-12-26 16:50:34 +0100

    first commit

---
+++ database/seeders/AdminsTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Admin;
+use Hash;
+
+class AdminsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $password = Hash::make('123456');
+        $adminRecords = [
+            ['id'=>1,
+             'name'=>'Admin',
+             'type'=>'admin',
+             'mobile'=>'08134284100',
+             'email'=>'immanuel@admin.com',
+             'password'=>$password,
+             'image'=>'',
+             'status'=>1
+            ],
+        ];
+        Admin::insert($adminRecords);
+    }
+}
